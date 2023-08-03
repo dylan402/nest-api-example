c502335c5065c80f2a17083a2c39f6f029b342df
Dylan Santiago <dylansantiagomelo@gmail.com>
2023-08-02 21:32:54 -0300

    Configurando config para Index Exporting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import httpConfig from './config/http.config';
+import { httpConfig } from './config';
 
 @Module({
   imports: [
